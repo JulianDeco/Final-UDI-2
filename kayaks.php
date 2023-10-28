41ded2e3c57ae401fef235c28a74f14f36083fa3
JulianDeco <juliandecoppet@gmail.com>
2023-10-28 12:45:52 -0300

    cambios kayaks .php

--- kayaks.php
+++ kayaks.php
@@ -1,3 +1,79 @@
+<?php
+
+session_start();
+// Conexión a la base de datos
+$conexion = new mysqli("localhost", "root", "", "registro-kayaks");
+
+// Verificar la conexión
+if ($conexion->connect_error) {
+    die("Error en la conexión a la base de datos: " . $conexion->connect_error);
+}
+
+// Función para agregar un nuevo registro
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["NOMBRE"])) {
+    $nombreArchivo = $_FILES['FOTO']['name'];
+    $tipoArchivo = $_FILES['FOTO']['type'];
+    $tamañoArchivo = $_FILES['FOTO']['size'];
+    $archivoTemporal = $_FILES['FOTO']['tmp_name'];
+
+    // Verificar que sea una imagen
+    if (strpos($tipoArchivo, 'image') !== false) {
+        // Mueve el archivo temporal a una ubicación deseada
+        $directorioDestino = 'uploads/';
+        $rutaArchivo = $directorioDestino . $nombreArchivo;
+
+        if (move_uploaded_file($archivoTemporal, $rutaArchivo)) {
+            echo "La imagen se ha guardado con éxito en $rutaArchivo.";
+        } else {
+            echo "Error al guardar la imagen.";
+        }
+    } else {
+        echo "El archivo seleccionado no es una imagen válida.";
+    }
+    // Obtener datos del formulario POST
+    $modelo = $_POST["MODELO"];
+    $tipo = $_POST["TIPO"];
+    $color = $_POST["COLOR"];
+    $cliente = $_POST["CLIENTE"].explode(" ");
+    $fecha_alta = $_POST["FECHA_ALTA"];
+    $fecha_baja = $_POST["FECHA_BAJA"];
+
+    $dni_cliente = $cliente[1];
+
+    // Crear la consulta SQL para insertar un nuevo registro en la tabla "registros"
+    $sql = "INSERT INTO kayak_kayaks (modelo, tipo, color, cliente, foto, fecha_alta, fecha_baja) VALUES ('$modelo', '$tipo', '$color', (SELECT kayak_clientes.id FROM kayak_clientes WHERE dni = '$dni_cliente'), ''$rutaArchivo, '$fecha_alta', '$fecha_baja')";
+    // Ejecutar la consulta y verificar si fue exitosa
+    if ($conexion->query($sql) === TRUE) {
+        // Redirigir de vuelta a la página actual después de agregar un registro
+        header("Location: " . $_SERVER['PHP_SELF']);
+    } else {
+        echo "Error: " . $sql . "<br>" . $conexion->error;
+    }
+}
+
+// Función para eliminar un registro
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["eliminar"])) {
+    // Obtener el ID del registro a eliminar del formulario POST
+    $id = $_POST["eliminar"];
+
+    // Crear la consulta SQL para eliminar un registro basado en su ID
+    $sql = "DELETE FROM kayak_clientes WHERE id=$id";
+    $conexion->query($sql);
+
+    // Redirigir de vuelta a la página actual después de eliminar un registro
+    header("Location: " . $_SERVER['PHP_SELF']);
+}
+
+
+
+// Consulta para obtener todos los registros
+$sql ="SELECT id, modelo, tipo, color, cliente, fecha_alta_fecha_baja FROM kayak_kayaks";
+$resultado = $conexion->query($sql);
+
+// Cerrar la conexión a la base de datos
+$conexion->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -48,6 +124,178 @@
         </div>
         </nav>
     </header>
+    <main class="content m-3">
+        <h1 class="text-center">Registros de Kayak</h1>
+
+        <!-- Formulario para agregar un nuevo registro -->
+        <form method="post" action="kayaks.php" class="d-flex flex-column align-items-center">
+            <div class="row w-75">
+                <div class="col-md-4">
+                    <div class="form-group" style="margin-bottom: 15px;">
+                        <label for="DNI">MODELO:</label>
+                        <input class="form-control" type="text" id="MODELO" name="MODELO" required>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group" style="margin-bottom: 15px;">
+                        <label for="TIPO">TIPO:</label>
+                        <input class="form-control" type="text" id="TIPO" name="TIPO" required>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group" style="margin-bottom: 15px;">
+                        <label for="COLOR">COLOR:</label>
+                        <input class="form-control" type="text" id="COLOR" name="COLOR">
+                    </div>
+                </div>
+            </div>
+            <div class="row w-75">
+                <div class="col-md-4">
+                    <div class="form-group" style="margin-bottom: 15px;">
+                        <label for="FOTO" class="form-label">FOTO:</label>
+                        <input class="form-control" type="file" id="FOTO" name="FOTO">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group" style="margin-bottom: 15px;">
+                        <label for="FECHA_ALTA">FECHA DE ALTA:</label>
+                        <input class="form-control" type="date" id="FECHA_ALTA" name="FECHA_ALTA">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group" style="margin-bottom: 15px;">
+                        <label for="FECHA_BAJA">FECHA DE BAJA:</label>
+                        <input class="form-control" type="date" id="FECHA_BAJA" name="FECHA_BAJA">
+                    </div>
+                </div>
+            </div>
+            <div class="row w-75 d-flex justify-content-center align-items-center">
+                <div class="col-md-4">
+                    <div class="form-group" style="margin-bottom: 15px;">
+                        <label for="CLIENTE">CLIENTE:</label>
+                        <div class="input-group mb-3">
+                            <select class="form-select" id="CLIENTE" name="CLIENTE">
+                                <option selected>-</option>
+                                <?php 
+
+                                $sql = "SELECT nombre, dni from kayak_clientes";
+
+                                $conexion2 = new mysqli("localhost", "root", "", "registro-kayaks");
+                                $clientes = $conexion2->query($sql);
+            
+                                while ($fila = $clientes->fetch_assoc()) {
+                                    echo "<option value='" . $fila["dni"] . "'>" . $fila["nombre"] . " " . $fila["dni"]  . "</option>";
+                                }
+                                ?>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row w-75 d-flex justify-content-center align-items-center">
+                <div class="col-md-4">
+                    <button type="submit" class="btn btn-primary w-100" name="guardar">Guardar</button>
+                </div>
+            </div>
+        </form>
+
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" class="m-3 d-flex flex-column align-items-center">
+            <div class="row">
+                <div class="col-md-8">
+                    <label for="search">BUSCAR POR TIPO:</label>
+                    <input class="form-control" type="search" id="search" name="search">
+                </div>
+                <div class="col-md-4 py-2">
+                    <br>
+                    <button type="submit" class="btn btn-primary btn-block" name="searchbtn">Buscar</button>
+                </div>
+            </div>
+        </form>
+
+        <!-- Lista de registros -->
+        <table class="table m-3">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Modelo</th>
+                    <th scope="col">Tipo</th>
+                    <th scope="col">Color</th>
+                    <th scope="col">Cliente</th>
+                    <th scope="col">Fecha alta</th>
+                    <th scope="col">Fecha baja</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["search"])) {
+
+                    $conexion = new mysqli("localhost", "root", "", "registro-kayaks");
+                    $search = $_POST["search"];
+
+                    $sql = "SELECT id, modelo, tipo, color, cliente, fecha_alta_fecha_baja FROM kayak_kayaks
+                    INNER JOIN kayak_estados as K_E
+                    ON K_E.id = kayak_clientes.estado 
+                    WHERE dni LIKE '%$search%'";
+                    
+                    $resultado_search = $conexion->query($sql);
+
+                    while ($fila = $resultado_search->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $fila["id"] . "</td>";
+                        echo "<td>" . $fila["modelo"] . "</td>";
+                        echo "<td>" . $fila["tipo"] . "</td>";
+                        echo "<td>" . $fila["color"] . "</td>";
+                        echo "<td>" . $fila["cliente"] . "</td>";
+                        echo "<td>" . $fila["descripcion"] . "</td>";
+                        echo "<td>" . $fila["fecha_alta"] . "</td>";
+                        echo "<td>" . $fila["fecha_baja"] . "</td>";
+                        // Crear un formulario para eliminar un registro con un botón "Eliminar"
+                        echo "<td>
+                        <form method='post' action='" . $_SERVER['PHP_SELF'] . "'>
+                            <input type='hidden' name='eliminar' value='" . $fila["id"] . "'>
+                            <input type='submit' value='Eliminar' class='btn btn-primary w-50'>
+                        </form>
+                        <form method='post' action='procesar_cliente_formulario.php'>
+                            <input type='hidden' name='modificarBtn' value='" . $fila["id"] . "'>
+                            <button type='submit' class='btn btn-warning w-50 my-2' data-bs-toggle='modal' data-bs-target='#exampleModal'>Modificar</button>
+                        </form>
+
+                    </td>";
+                        echo "</tr>";
+                    }
+                } else if ($resultado->num_rows > 0) {
+                    while ($fila = $resultado->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $fila["id"] . "</td>";
+                        echo "<td>" . $fila["modelo"] . "</td>";
+                        echo "<td>" . $fila["tipo"] . "</td>";
+                        echo "<td>" . $fila["color"] . "</td>";
+                        echo "<td>" . $fila["cliente"] . "</td>";
+                        echo "<td>" . $fila["descripcion"] . "</td>";
+                        echo "<td>" . $fila["fecha_alta"] . "</td>";
+                        echo "<td>" . $fila["fecha_baja"] . "</td>";
+                        // Crear un formulario para eliminar un registro con un botón "Eliminar"
+                        echo "<td>
+                        <form method='POST' action='" . $_SERVER['PHP_SELF'] . "'>
+                            <input type='hidden' name='eliminar' value='" . $fila["id"] . "'>
+                            <input type='submit' value='Eliminar' class='btn btn-primary w-75'>
+                        </form>
+                        <form method='POST' action='procesar_cliente_formulario.php'>
+                            <input type='hidden' name='modificarBtn' value='" . $fila["id"] . "'>
+                            <button id='btnModificarJS' type='submit' class='btn btn-warning my-2' >Modificar</button>
+                        </form>
+                        </td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<p class=text-center>No hay registros en la base de datos.</p>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </main>
 
     <footer class="py-3 my-4">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
